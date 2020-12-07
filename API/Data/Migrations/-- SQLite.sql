94c8b780226118bf7c5d62a94ef08af8e207f9a8
Sai4445 <sainikhilmaringanti@gmail.com>
2020-12-07 11:37:02 +0000

    End of section 2

---
+++ API/Data/Migrations/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"nikhil");
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2,"RAMA");
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3,"SITA");
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (4,"LAKSHMAN");
