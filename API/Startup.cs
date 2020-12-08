711936383a5cd291596a9966eb07359876e8c3c1
Sai4445 <sainikhilmaringanti@gmail.com>
2020-12-08 14:04:27 +0000

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,7 @@ namespace API
 
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
