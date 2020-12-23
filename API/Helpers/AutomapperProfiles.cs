149eb0513e2dd1e66ec23ce653e179e9f4ba8845
Sai4445 <sainikhilmaringanti@gmail.com>
2020-12-23 13:46:37 +0000

    end of section 8

---
+++ API/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extentions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutomapperProfiles : Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest=>dest.PhotoUrl, opt=>opt.MapFrom(src =>src.Photos.FirstOrDefault(x=>x.IsMain).Url))
+            .ForMember(dest =>dest.Age,opt =>opt.MapFrom(src=>src.DateOfBirth.CaluclateAge()));
+
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
