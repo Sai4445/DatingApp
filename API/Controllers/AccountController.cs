6f4b5d3b9f193d90a3dffd9e63de86483873aeb4
Sai4445 <sainikhilmaringanti@gmail.com>
2020-12-29 14:26:51 +0000

    end of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -50,7 +50,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
             if (user == null) return Unauthorized("invalid username");
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
@@ -61,7 +61,7 @@ namespace API.Controllers
               return new UserDto{
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x =>x.IsMain)?.Url
+                photoUrl = user.Photos.FirstOrDefault(x =>x.IsMain)?.Url
             };
         }
         private async Task<bool> UserExits(string UserName)
--- API/Extentions/ApplicationServiceExtensions.cs
+++ API/Extentions/ApplicationServiceExtensions.cs
@@ -1,4 +1,4 @@
-using API.Data;
+ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
